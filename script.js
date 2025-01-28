1bdd6b251ea50046753bfef11e2edb00788723bc
kauamassei <gameplayultra11@gmail.com>
2025-01-28 17:30:34 -0300

    Sessão Personagens completa + requisição da api funcionando

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-const URL = 'https://zelda.fanapis.com/api/games';
+const URL = 'https://zelda.fanapis.com/api/characters?limit=100';
+const characterContainer = document.getElementById('characters');
+
 
 async function fetchAPI() {
     const response = await fetch(URL)
 
     if (response.status === 200) {
-        const games = await response.json();
-        console.log(games);
+        const data = await response.json();
+        displayCharacters(data.data);
+        
+        console.log(data);
 
     } else {
         console.log('Erro na requisição.')
+        characterContainer.innerHTML = `<p>Não foi possível carregar essas informações</p>`
     }
+}
+
+function displayCharacters(characters) {
+    characterContainer.innerHTML = '';
+    characters.forEach(character => {
+        const characterCard = document.createElement('div');
+        characterCard.classList.add('character-card');
+
+        characterCard.innerHTML = `
+        <h3 class="npcName">${character.name}</h3>
+        <p class='pOutros'>${character.description || "Sem descrição disponível"}</p>
+        <p class= "pGender">Tipo: ${character.race || "Sem descrição disponível"}</p>
+        <p class="pRace">Sexo: ${character.gender || "Sem descrição disponível"}</p>
+        
+        `;
+
+        characterContainer.appendChild(characterCard)
+    });
 }
 
 fetchAPI()
